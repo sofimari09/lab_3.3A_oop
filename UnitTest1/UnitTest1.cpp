139df3f1337545121b41603890e9f87e2a3e17bc
Lenovo <Desofiyamariya@gmail.com>
2022-06-17 14:31:35 +0300

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PUBrat.cpp"
+#include "../Rational.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			PUBrat p{ 1,2 };
+			p--;
+			Assert::AreEqual(p.geta(), 0);
+
+		}
+	};
+}
